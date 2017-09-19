745253e1aca741926e9689c24d43bdda189bbd0d
Kevin Yeap <paeynivek@gmail.com>
2017-09-18 21:41:10 -0700

    changed html files to php pulled out navigation menu to seperate file

--- home.php
+++ home.php
@@ -5,25 +5,7 @@
   <link rel="stylesheet" href="blog.css">
 </head>
 <body>
-
-
-  <ul>
-    <li><a href="home.html">Home</a></li>
-    <li><a href="resume.html">Resume</a></li>
-    <li><a href="#projects">Projects</a></li>
-  </ul>
-
-  <nav>
-    <h1>Kevin Yeap</h1>
-    <img src="./images/profile_pic.png" alt="Profile Picture">
-
-    <a href="https://www.linkedin.com/in/kyeap/"> Linkedin</a><br>
-    <a href="https://github.com/paeynivek">Github</a><br>
-    <a href="http://steamcommunity.com/id/paeyn">Steam</a><br>
-    <a href="https://www.facebook.com/paeynivek">Facebook</a><br>
-    <a href="https://www.hackerrank.com/paeyn">Hackerrank</a><br>
-
-  </nav>
+  <?php include("./navigation_menu.html"); ?>
 
   <p>This is my github blog</p>
 
--- resume.php
+++ resume.php
@@ -5,33 +5,11 @@
   <link rel="stylesheet" href="blog.css">
 </head>
 <body>
+  <?php include("./navigation_menu.html"); ?>
 
+  <div class="resume_pdf">
+    <embed src="./resources/Kevin Yeap Resume 2017.pdf" width="800px" height="1100px" />
+  </div>
 
-  <ul>
-    <li><a href="home.html">Home</a></li>
-    <li><a href="resume.html">Resume</a></li>
-    <li><a href="#projects">Projects</a></li>
-  </ul>
-
-
-  
-
-
-  <nav>
-    <h1>Kevin Yeap</h1>
-    <img src="./images/profile_pic.png" alt="Profile Picture">
-
-    <a href="https://www.linkedin.com/in/kyeap/"> Linkedin</a><br>
-    <a href="https://github.com/paeynivek">Github</a><br>
-    <a href="http://steamcommunity.com/id/paeyn">Steam</a><br>
-    <a href="https://www.facebook.com/paeynivek">Facebook</a><br>
-    <a href="https://www.hackerrank.com/paeyn">Hackerrank</a><br>
-
-  </nav>
-    
-    <div class="resume_pdf">
-      <embed src="Kevin Yeap Resume 2017.pdf" width="800px" height="1100px" />
-    </div>
-    
-  </body>
+</body>
 </html>
