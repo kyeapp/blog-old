e01e0824b187a75ae1985ea7d33fe102c6381ea2
Kevin Yeap <paeynivek@gmail.com>
2017-09-19 23:23:44 -0700

    separated out header, navigation, footer fixed unordered list overflow margin fixed alignment cahnge when scrollbar appears/disappears

--- home.php
+++ home.php
@@ -5,9 +5,26 @@
   <link rel="stylesheet" href="blog.css">
 </head>
 <body>
-  <?php include("./navigation_menu.html"); ?>
+  <div id="page">
+    <div id="header"><?php include("./header.html"); ?></div>
 
-  <p>This is my github blog</p>
+    <div id="container">
+      <div id="sidebar"><?php include("./navigation.html"); ?></div>
 
+      <div id="content">
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus ac nisl. Fusce egestas. Praesent vitae neque. Phasellus ultricies, lacus quis sagittis vulputate, nisi tellus laoreet libero, vitae suscipit urna urna ullamcorper augue. Phasellus enim. Fusce est tortor, cursus sit amet, blandit id, sollicitudin a, pede. Sed eu massa id mauris aliquam placerat. Curabitur turpis. Nulla id tellus. Praesent dolor odio, tincidunt sit amet, vehicula nec, ornare nec, lectus. Vestibulum rhoncus quam sed leo. Quisque leo odio, venenatis et, dictum non, suscipit ut, lacus. Duis eleifend, metus imperdiet sagittis scelerisque, nibh eros dictum arcu, in aliquam est tortor nec mi. Sed quis massa vitae mauris ullamcorper faucibus.</p>
+      </div>
+    </div>
+    
+    <div id="footer"><?php include("./footer.html"); ?></div>
+  </div>
 </body>
 </html>
+
+
+
+<!-- sublime shortcuts
+crtl + L .......... highlight entire line
+crtl + / .......... comment highlighted
+crtl + shift + k .. delete line
+ -->
--- resume.php
+++ resume.php
@@ -5,11 +5,21 @@
   <link rel="stylesheet" href="blog.css">
 </head>
 <body>
-  <?php include("./navigation_menu.html"); ?>
+  <div id="page">
+    <div id="header"><?php include("./header.html"); ?></div>
 
-  <div class="resume_pdf">
-    <embed src="./resources/Kevin Yeap Resume 2017.pdf" width="800px" height="1100px" />
+    <div id="container">
+      <div id="sidebar"><?php include("./navigation.html"); ?></div>
+
+      <div id="content">
+        <embed src="./resources/Kevin Yeap Resume 2017.pdf" width="864px" height="1140px" />
+      </div>
+    </div>
+    
+    <div id="footer"><?php include("./footer.html"); ?></div>
   </div>
-
 </body>
 </html>
+
+
+
