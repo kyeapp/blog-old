9abc6db13db945a3e3c4a682615444d1b73e3f80
Kevin Yeap <paeynivek@gmail.com>
2017-09-22 04:33:42 -0700

    added navigation icon and animation

--- resume.php
+++ resume.php
@@ -5,7 +5,6 @@
   <link rel="stylesheet" href="blog.css">
 </head>
 <body>
-
   <div id="page">
     <div id="header"><?php include("./header.html"); ?></div>
 
@@ -13,7 +12,6 @@
       <div id="sidebar"><?php include("./navigation.html"); ?></div>
 
       <div id="content">
-
         <embed src="./resources/Kevin Yeap Resume 2017.pdf" width="864px" height="1140px"/>
       </div>
     </div>
